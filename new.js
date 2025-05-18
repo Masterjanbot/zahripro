cc8cb787315ee3c8ea8fcb8833b3a77b576d5a8b
zahir sahib <ghaznighaznawi797@gmail.com>
2025-05-17 22:24:03 -0700

    اضافه کردن فایل‌های ابتدایی پروژه

---
+++ new.js
@@ -0,0 +1,695 @@
+// const age1=function (birthyear){
+//    return 237-birthyear;
+// }
+// const age2= birthyear >=237-birthyear;
+// const age3= age1(197);
+// console.log(ag3);
+
+
+
+// function myFunc(theObject) {
+//    theObject.make = "Toyota";
+//  }
+ 
+//  const myCar = {
+//    make: "Honda",
+//    model: "Accord",
+//    year: 1998,
+//  };
+ 
+//  console.log(myCar.make); // "Honda"
+//  myFunc(myCar);
+//  console.log(myCar.make); // "Toyota"
+ 
+
+
+//  function myfunc(theobject) {
+//    thobject.make = "Totaly"
+//  }
+
+//  const mycar ={
+//    make: "Honda",
+//    model:"Accord",
+//    year:1998,
+//  }
+//  console.log(mycar.make);
+//  myfunc(mycar)
+//  console.log(mycar.make)
+
+//  function myFunc(theArr) {
+//   theArr[0] = 30;
+// }
+
+// const arr = [45];
+
+// console.log(arr[0]); // 45
+// myFunc(arr);
+// console.log(arr[0]); // 30
+
+
+// const square = function (number) {
+//   return number * number;
+// };
+
+// console.log(square(4)); // 16
+
+// const zarb = function(number){
+//   return number+number;
+
+// };
+
+// console.log(zarb(12))
+
+
+
+
+// function map(f, a) {
+//   const result = new Array(a.length);
+//   for (let i = 0; i < a.length; i++) {
+//     result[i] = f(a[i]);
+//   }
+//   return result;
+// }
+
+
+
+// function factorial(n) {
+//    if (n === 0 || n === 1) {
+//      return 1;
+//    } else {
+//      return n * factorial(n - 1);
+//    }
+//  }
+//  console.log(factorial(1))
+//  console.log(factorial(3))
+//  console.log(factorial(9))
+  
+
+// function declertion
+
+// function zahirjan(){
+//    console.log("Master zahir jan is good and so smalling studing ")
+//    console.log("what you think about zahir jan ")
+//    console.log("are you shur zahir jan is good student")
+// }
+
+// zahirjan();
+// zahirjan();
+// zahirjan();
+
+
+// functoin expertion .
+
+// var Master= function() {
+//    console.log("zahir jan is best developer and webdesginer")
+//    console.log("Master want to improve the feuter ok")
+//    console.log("don,t worry about this think ok .")
+// }
+
+// Master();
+// Master();
+
+// const addnumber= function(num1 ,num2){
+//    console.log(num1 +num2)
+//    console.log(num1*num2)
+//    console.log(num1/num2)
+// }
+// addnumber(12,12);
+// addnumber(123,123)
+// addnumber(142,1233)
+
+// const add= function(sm1,sm2){
+//    return sm1+sm2;
+// }
+// const x=add(12,45)
+// console.log(x);
+
+
+// const text=function(nk2,q2){
+//    return nk2+q2;
+// }
+// const x=text(12,34);
+// console.log(x)
+
+
+
+// var num1=12;
+// var num2=14;
+// var num3=14;
+// var num4=12;
+
+// var total=num1+num2+num3+num4;
+// var avrg= total/4;
+
+// if (avrg >20){
+//    console.log("you acsept this exime");
+
+// }else{
+//    console.log("you not acsept this exim");
+// }
+
+
+// function avrge(num1,num2,num3,num4){
+//    var add=num1+num2+num3+num4;
+//    var av=add/4;
+
+//    if (av>30){
+
+//       return "good"
+//    }
+      
+//       else {
+//          return "not good"
+         
+//       }
+
+// }
+// var state =avrge(90,90,90,90)
+
+
+
+// console.log(avrge(20,80,90,80))
+// console.log(avrge(20,30, 35,55))
+// console.log(avrge(20 ,12, 40,18))
+
+// const add =function(h1,h2,h3,h4,h5){
+//    const x=h1+h2+h3+h4+h5;
+//    const z=x*12;
+//    console.log(z)
+
+// }
+// add(12,12,12,12,12);
+
+
+// function sum (...numbers){
+//    var totol=2;
+//    for (var i=0; i < numbers.length;i++){
+//       totol+=numbers[i];
+//    }
+
+//    return totol;
+// }
+// console.log(sum(12,34));
+ 
+// function myfunction(){
+//    console.log(arguments.length)
+  
+// }
+// myfunction(23,56,34)
+
+
+
+// this function is not solve ?
+
+
+// function sum(){
+//    var total=0;
+//    for(var i=0; i < arguments.lenght ; i++){
+//       total +=arguments[i];
+//    }
+//    return total;
+// }
+
+// console.log(sum());
+// console.log(sum(5));
+// console.log(sum(5, 10));
+// console.log(sum(12,34,56))
+
+
+// function sum(num1, num2 =0){
+//    return num1 + num2;
+// }
+
+// console.log(sum(12,2))
+// console.log(sum(12,23))
+// console.log(sum(12,45))
+ 
+
+//  let myarry=[5,20,18,22];
+
+// // for (let i=0 ;i<myarry.length;i++){
+// //    console.log(myarry[i]); 
+
+// // }
+
+// for (let x of myarry){
+//    console.log(x)
+// }
+
+
+// let str ='maste zahir jan aziz'
+// for (let element of str ){
+//    console.log (element)
+// }
+
+
+// for (var num = 1; num <=6; num++){
+//    console.log(num)
+// }
+
+// for (let add=0 ; add<=12 ; add++){
+//    console.log(add)
+// }
+
+// for (let ww=2 ; ww<=20; ww++){
+//    console.log(ww)
+// }
+ 
+
+//  let sum=0;
+// for (let num=10 ; num<=89; num++){
+//   sum +=num;
+// }
+
+// console.log(sum);
+
+
+// var add=0 , yan=20;
+
+// for (var x=1; x<=yan; x++){
+
+//   if ( yan%x ==0)
+//     add++;
+// }
+
+// if (add==6){
+//   console.log("misha oh")
+// }else
+// console.log("na nmisha oh")
+
+// while (true){
+//   console.log('zahir jan is good boy ')
+// }
+
+// for (var a=1; a<=6; a++){
+//   console.log('outer'+a)
+  
+//     for(var b=1; b<=5b n;b++)
+//       {
+//       console.log('inner'+b)
+    
+//   }
+// }
+
+//  var str='';
+
+// for (var i=1; i<=6; i++){
+//   for (var j=1 ; j<=i ; j++) 
+//     { 
+  
+//     str  +='*';
+//   }
+//   str +='\n';
+// }
+// console.log(str);
+
+// var grades =[12, 3, 12, 45 , 23] ; 
+
+// var sum =grades[0]+grades[1]+grades[2]+grades[3]+grades[4];
+
+// var avrge = sum/grades.length;
+ 
+// console.log(avrge);
+
+// var a=[12 ,'zahir', 23]
+// console.log(a)
+
+// var b =['']
+
+// b[0]=89;
+// b[1]=12;
+// b[3]=56;
+
+// console.log(b.length);
+
+
+// var num=[17 ,20 , 24 ,31, 44]
+
+// for (var i=0 ; i<=4; i++){
+//   var z= num [i]*2;
+//     // console.log(num[i]);
+  
+// }
+
+// console.log(z)
+
+// var w =17;
+// var s=17;
+
+// var t =w *s;
+// console.log(t)
+
+
+// var gring =[12, 12, 45, 4  ,46 , 23 ,12 ,34  , 56 ]
+
+// var sum=0;
+
+// for ( var i=0 ; i<=8; i++){
+//   sum +=[i];
+// }
+// var avrge = sum /8;
+// console.log(avrge)
+
+// var worint =[12, 34, 'zahir' , 'qestion', true ,34 ]
+// var l= worint.push('zahir jan is good')
+
+// worint.shift();
+// worint.unshift('baise')
+// console.log(worint.toString())
+// console.log(worint.join('+'))
+
+// console.log(l)
+
+// delete worint[3 ]
+// delete worint[0]
+// console.log(worint)
+
+// var array1=[12 ,34, 43 , 12 ,45 , 10];
+
+// var array2=[34 ,12, 43, 10 , 67];
+
+// var array3=array1.concat(array2);
+
+// console.log(array3)
+
+// sort is a method به ترتیب می کند کد را
+// reverse code سرچپه می  کند کد را
+
+// var array1=['zahir', ' jan' , 'qand' ,' ok'];
+
+// array1.sort()
+
+// console.log(array1)
+
+// var arra2= ['rahim' , 'atarode' ,23 , 89, ' zahir'];
+// arra2.reverse();
+
+// console.log(arra2.lastIndexOf())
+
+// var array1= [10 ,14.5, 16, 18, 75, 23];
+
+// var max=0;
+// for (var i=0 ; i<array1.length; i++){
+//    if (array1[i] >max){
+//       max = array1[i];
+//    }
+// }
+
+
+
+
+//  function  
+
+
+// function names(){
+//    console.log('zahir jan')
+//    console.log('sahibi ')
+//    console.log('ok ths ok');
+// }
+// names()
+// names()
+// names()
+
+
+// var zahir = function(){
+//    console.log(' I,m ok don,t warry about me ok ')
+//    console.log(' whats up anythings ok ?')
+//    console.log('yes any thing ok ua')
+// }
+
+// zahir();
+// zahir();
+
+
+// function addnumber(num1 , num2){
+//    return num1 +num2;
+
+// }
+// var x =addnumber(34,45);
+// console.log(x)
+
+
+// function add(S ,D){
+//    return S +D;
+// }
+// var w= add(34 , 78);
+// console.log(w)
+
+
+// var r =function(b, c ){
+//    return b -c;
+
+// }
+
+// var e=r(12, 45);
+// console.log(e)
+
+// number1 function declertion ;
+
+// number2 functin expretion;
+
+
+
+
+
+// function avrge(num1 ,num2 , num3, num4, ){
+//    var total = num1+num2+num3+num4;
+//    var av= total/4;
+    
+//    if (av>=20){
+//       return  'he is pass';
+//    }
+
+//    else{
+//       return ' he is loose'
+//    }
+
+// }
+
+// console.log(avrge(12, 45 ,14, 67 ))
+// console.log(avrge(90 , 45, 10 ,23))
+
+
+// // nothing
+
+// function avrage(num1 , num2, num3, num4){
+//    var total = num1+num2+num3+num4;
+//    var at= total/4;
+
+//    if (at >=55){
+//       return 'it id paadiong ok'
+
+//    }
+//    else{
+//       return 'it is doesn,t paasing'
+//    }
+
+// }
+
+// console.log(avrage(89, 12 , 45 ,12))
+// console.log(avrage(67, 89 ,90 ,80))
+
+
+
+// it is also litale deficult 🤔
+
+
+// function sum(){
+//    var total =0;
+//    for (var i=0 ; i<arguments.length; i++){
+//         total +=arguments[i];
+
+//    }
+//    return total ;
+// }
+
+// console.log(sum());
+// console.log(sum(23 ,45 ,56))
+
+
+
+// I,m don,t understand this code?🤔
+
+// function sum(...numbers){
+//    var total=0;
+
+//    for (var i=0 ; i<numbers.length;i++){
+//         total +=numbers[i];
+      
+//    }
+
+//    return total;
+
+// }
+
+// console.log(sum(223,12))
+
+// var result =1;
+// for (var i=1; i<4 ; i++){
+//    result *=2;
+
+// }
+// console.log(result);
+
+
+// function power(base ,exponent){
+//    var result =1;
+
+//    for (var i=1 ; i<=exponent; i++){
+//       result *=base;
+
+//    }
+//    return result
+// }
+// console.log(power(2,5))
+// console.log(power(23,12))  😍
+
+
+
+// it is callback fuction ok 😘
+
+
+//  function firstfunction(callback){
+
+//    console.log('zahir jan is ok')
+//    callback()
+//  }
+
+//  firstfunction(secondfunction)
+  
+// function secondfunction(){
+
+//    console.log('is ahhmad ok ')
+
+
+// var massage = () => console.log('enter zahir na djkdsnsdnf;lklnkfdoixzcnei');
+// massage();
+
+// var zahir =(a ,b ,w)=> a+b*w;
+
+// console.log(zahir(23, 45 ,9))
+
+
+// var power =(num) => num*num;
+// console.log(power(12))
+// console.log(power(23))
+
+
+//it is scop function
+// var a =5;
+// function mycunc(){
+//   if (true){
+//    console.log(ab )
+//   }
+
+// }
+// console.log(a)
+// mycunc
+
+//this cod is hiosthing
+
+// website ='next1code'
+// console.log(website);
+// var website;
+// zahir()
+//  function zahir(){
+//   console.log('zahir')
+// }
+
+
+// function greet(name) {
+//   console.log(`Hello ${name}` );
+// }
+
+// // pass "John" as argument
+// greet("John");
+
+// // pass "David" as argument
+// greet("David");
+
+
+// function myfunction(names){
+//   console.log(`Helo ${ names}`)
+// }
+
+// myfunction()
+
+
+
+// function display() {
+
+//   console.log("This will be executed.");
+
+//   return "Returning from function.";
+
+//   console.log("This will not be executed.");
+// }
+
+// let message = display();
+// console.log(message);
+
+
+
+// const mailad_ghazni={
+//   age :23,
+//   name:'milad jan',
+//   addres: 'ghazni',
+//   last_name: 'zahir',
+//   hieght:89,
+//   color:['red', 'green' , 'belue']
+// }
+
+// console.log(mailad_ghazni.age);
+// console.log(mailad_ghazni.hieght);
+// console.log(mailad_ghazni.name);
+// console.log(mailad_ghazni.addres);
+// console.log(mailad_ghazni.color);
+
+// const zarif={
+//   first : 'zahir',
+//   last_namee:'sahbi',
+//   age: 34,
+//   coding: function(){
+//     return 'zahir is a ok boy yes don,t worry';
+//   },
+
+//    grades:{
+//     dari:45,
+//     math:60,
+//     geomitry:34
+
+// },
+
+// reworda:{
+//   kindel:'full',
+//   behave:'nothing'
+
+// }
+// }
+// console.log(zarif.first);
+// console.log(zarif.coding)
+// console.log(zarif.grades.math)
+// console.log(zarif.reworda.kindel)
+
+// let myclass=document.getElementById("mymain");
+// myclass.style.color='green'
+
+// console.log(myclass)
+
+// let zahir=document.getElementById('zahir')
+
+// console.log(zahir.innerHTML)
+
+// let muinput=document.getElementById('myinput');  
+// muinput.value=('zahir jan')
+// console.log(muinput)  
+
+let divcontent=prompt('enter your name please');
+
+let zahirjan=document.createElement("div");
+zahirjan.setAttribute('class', 'zahir');
+
+zahirjan.innerHTML=divcontent;
+console.log(zahirjan);
+
+
